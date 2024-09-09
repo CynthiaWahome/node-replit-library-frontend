2731144ae2c633de4e43101af93e13ecce8d5400
Cynthia <160864573+CynthiaWahome@users.noreply.github.com>
2024-09-09 00:04:04 +0000

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,44 @@
+import { useState } from 'react';
+import axios from 'axios';
+import './App.css';
+
+axios.defaults.baseURL = "https://46c02a64-23e3-414a-bca7-58a80c8db2df-00-3cpbtytslpj6y.worf.replit.dev"
+import { collapseTextChangeRangesAcrossMultipleVersions } from 'typescript';
+
+export default function App() {
+  const [books, setBooks] = useState([]);
+  const fetchBooks = async () => {
+    try {
+      const response = await axios.get("/api/books");
+      const fetchedBooks = response.data.map((bookObj) => bookObj.value);
+      setBooks(fetchedBooks);
+    } catch (error) {
+      console.error("Error fetching books: ", error);
+    }
+  };
+
+  // console.log(fetchBooks());
+
+
+  return (
+    <main className='app'>
+      <h1>Book Library</h1>
+      <hr/>
+
+      <div>
+        <div className="left-side">
+          <h2>All Books</h2>
+          <button onClick={fetchBooks}>Get All Books</button>
+          <ul>
+            {books.map((eachBook) => (
+            <li key={eachBook.id}>
+              {eachBook.id}) {eachBook.title} - {eachBook.author} 
+            </li>
+            ))}
+          </ul>
+          
+        </div>
+      </div>
+    </main>
+  )
+}
